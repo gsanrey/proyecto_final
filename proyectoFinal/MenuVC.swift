728a6724af556bb24eefe9484dc61c7b8044a5c2
Gabriel Urso Santana Reyes <brielsantana@gmail.com>
2016-12-30 07:12:05 +0000

    menu de inicio

---
+++ proyectoFinal/MenuVC.swift
@@ -0,0 +1,29 @@
+//
+//  MenuVC.swift
+//  proyectoFinal
+//
+//  Created by Gabriel Urso Santana Reyes on 30/12/16.
+//  Copyright © 2016 Gabriel Urso Santana Reyes. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
